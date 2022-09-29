7bdcd9fa38d3729772c5c56cd9a813fd3fe196a2
Bruno Nogueira <bruno.nogueiratt@gmail.com>
2022-09-29 11:01:29 -0300

    componentes react

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Home.css';
+
+function Home() {
+    return (
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://portalguara.com/wp-content/uploads/2022/02/pica-pau-3.jpg" alt="pica pau" className="img"/>
+        </>
+    );
+}
+
+export default Home;
