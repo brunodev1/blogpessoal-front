0edd09a5a860f895a8726d4b7778dd2c9f0ec404
Bruno Nogueira <bruno.nogueiratt@gmail.com>
2022-10-02 19:16:29 -0300

    refatorando css

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
 import React from 'react';
-import Navbar from './components/estaticos/navbar/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
 import './App.css';
 import Home from './paginas/home/Home';
+import Login from './paginas/login/Login'
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Home />
-      <Footer />
-    </>
-  );
+    
+      <BrowserRouter>
+        
+          <Navbar />
+  
+          <div style={{minHeight: '100vh'}}>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          
+          <Footer />
+        
+      </BrowserRouter>
+    );
 }
 
 export default App;
