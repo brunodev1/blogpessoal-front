f31307b98b5e7b8db895650206d1b4c4049f5b76
Bruno Nogueira <bruno.nogueiratt@gmail.com>
2022-10-18 21:53:24 -0300

    footer

--- src/components/estaticos/footer/Footer.tsx
+++ src/components/estaticos/footer/Footer.tsx
@@ -1,56 +1,78 @@
-import React from "react";
-import { Typography, Box, Grid } from '@material-ui/core';
+import { Box, Grid } from '@mui/material';
+import React from 'react';
 import InstagramIcon from '@material-ui/icons/Instagram';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import './Footer.css';
-import { useSelector } from "react-redux";
-import { TokenState } from "../../../store/tokens/tokensReducer";
+import CreditCardIcon from '@mui/icons-material/CreditCard';
+import PaidIcon from '@mui/icons-material/Paid';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+import './Footer.css'
+import img1 from '../../../imgs/carto.png'
+import img2 from '../../../imgs/paypal.png'
+import img3 from '../../../imgs/visa.png'
+import img4 from '../../../imgs/mastercard.png'
+
+
 
 
 function Footer() {
-
-    const token = useSelector<TokenState, TokenState["tokens"]>(
-        (state) => state.tokens
-      );
-
-      var footerComponent;
-
-      if (token !== "") {
-        footerComponent =  <Grid container direction="row" justifyContent="center" alignItems="center">
-        <Grid alignItems="center" item xs={12}>
-            <Box className="box1">
-                <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                    <Typography variant="h5" align="center" gutterBottom className="textos">Siga-nos nas redes sociais </Typography>
-                </Box>
-                <Box display="flex" alignItems="center" justifyContent="center">
-                    <a href="https://www.facebook.com/generationbrasil" target="_blank">
-                        <FacebookIcon className="redes" />
-                    </a>
-                    <a href="https://www.instagram.com/generationbrasil/" target="_blank">
-                        <InstagramIcon className="redes" />
-                    </a>
-                    <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank">
-                        <LinkedInIcon className="redes" />
-                    </a>
-                </Box>
-            </Box>
-            <Box className='box2'>
-                <Box paddingTop={1}>
-                    <Typography variant="subtitle2" align="center" gutterBottom className="textos" >© 2020 Copyright:</Typography>
-                </Box>
-                <Box>
-                    <a target="_blank" href="https://brasil.generation.org">
-                        <Typography variant="subtitle2" gutterBottom className="textos" align="center">brasil.generation.org</Typography>
-                    </a>
-                </Box>
-            </Box>
-        </Grid>
-    </Grid>
-      }
     return (
         <>
-        {footerComponent}
+            
+            <Grid container justifyContent='space-evenly'>
+                <Grid item xs={4} display='flex' flexDirection='column' alignItems='center' style={{backgroundColor: '#0D0013', color: "#ffffff", fontWeight: 700, textDecoration: 'none' }}>
+                    
+
+                    <h3>Informações</h3>
+                    
+                    <p>Sobre nós | Contato | Política de privacidade</p>
+            
+                    
+                </Grid>
+                <Grid item xs={4} display='flex' flexDirection='column' alignItems='center' style={{backgroundColor: '#0D0013', color: "#ffffff", fontWeight: 700 }}>
+                    <h3>Formas de pagamento</h3>
+
+                    <Grid flexDirection='row' className='icons2'>
+                        <img src={img4} className='img'/>
+                        <img src={img3} className='img'/>
+                        <img src={img2} className='img'/>
+                        <img src={img1} className='img'/>
+                    </Grid>
+                </Grid>
+                <Grid item xs={4} display='flex' flexDirection='column' alignItems='center' style={{backgroundColor: '#0D0013', color: "#ffffff", fontWeight: 700}}>
+                    <h3>Redes sociais</h3>
+
+                    <Grid className='icons'>
+                            
+
+                            <a href="https://www.facebook.com/generationbrasil" target="_blank" rel="noopener noreferrer">
+                                <FacebookIcon style={{ fontSize: 40, color: "white" }} />
+                            </a>
+                            
+                            <a href="https://www.instagram.com/generationbrasil/" target="_blank" rel="noopener noreferrer">
+                                <InstagramIcon style={{ fontSize: 40, color: "white" }} />
+                            </a>
+                            
+                            <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank" rel="noopener noreferrer">
+                                <LinkedInIcon style={{ fontSize: 40, color: "white" }} />
+                            </a>
+
+                            <a href="https://github.com/" target="_blank" rel="noopener noreferrer">
+                                <GitHubIcon style={{ fontSize: 40, color: "white" }}></GitHubIcon>
+                            </a>
+
+                    </Grid>
+
+                </Grid>   
+            </Grid>
+            
+            
+            
+            <Grid container display='flex' justifyContent='center' style={{fontWeight: 600}}>
+            Rufúgio da Moda | © 2022 Copyright: Todos os direitos reservados.
+            </Grid>
+
         </>
     )
 }
